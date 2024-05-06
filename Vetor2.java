a67a7862c227e7c8dc163a5610a67dcf3bad352e
Carlos-EduardoR <161321567+Carlos-EduardoR@users.noreply.github.com>
2024-05-06 10:37:29 -0300

    Add files via upload

---
+++ Vetor2.java
@@ -0,0 +1,35 @@
+package Entregas;
+
+import java.util.Scanner;
+
+public class Vetor2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] vet1 = new int[10];
+        int[] vet2 = new int[10];
+        int[] vet3 = new int[20];
+        Scanner sc = new Scanner(System.in);
+
+        int j = 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Insira o elemento " + (i + 1) + " do vetor 1: ");
+            vet1[i] = sc.nextInt();
+            vet3[j - 1] = vet1[i];
+            j++;
+
+            System.out.print("Insira o elemento " + (i + 1) + " do vetor 2: ");
+            vet2[i] = sc.nextInt();
+            vet3[j - 1] = vet2[i];
+            j++;
+        }
+
+        System.out.println("Vetor 3:");
+        for (int i = 0; i < 20; i++) {
+            System.out.print(vet3[i] + " ");
+        }
+        sc.close();
+	}
+
+}
